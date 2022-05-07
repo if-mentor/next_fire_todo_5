98ab7e262096e026549a8d1d96e9299b64931648
naokitime <naoki.time.1115@gmail.com>
2022-05-08 08:43:54 +0900

    #31 UIフレームワークの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </>
+  )
 }
 
 export default MyApp
