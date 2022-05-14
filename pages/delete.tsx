e5ff2ae004a2013f5ebb112b3a2f6997f0b8c458
nto300002 <samonkntd@gmail.com>
2022-05-14 22:19:05 +0900

    #22 feature: 削除ページ作成

--- pages/delete.tsx
+++ pages/delete.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { Box, Button, CssBaseline, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import { Box, Button, CssBaseline,  Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import { Header } from '../components/Header'
 import Container from '@mui/material/Container';
 
