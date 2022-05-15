e2f8fcf8bed32039922d6e0f1591d0205bbecbea
nto300002 <samonkntd@gmail.com>
2022-05-15 18:11:03 +0900

    Paginationの導入&表示の修正

--- pages/delete.tsx
+++ pages/delete.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { Box, Button, CssBaseline,  Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import { Box, Button, CssBaseline,  Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography,  Pagination } from '@mui/material'
 import { Header } from '../components/Header'
 import Container from '@mui/material/Container';
 
@@ -12,29 +12,30 @@ const detail: NextPage = () => {
         maxWidth="xl"
       >
       <CssBaseline />
-        <Box sx={{ textAlign: "right", marginLeft: "auto" }} >
-           <Button variant="contained" sx={{ pl: 4, pr: 4, mt: 3, mb: 2, mr: 2, background: "#ec7272", "&:hover": { background: "#ec7171", opacity: [0.9, 0.8, 0.7] }, borderRadius: 25, borderColor: 'text.primary', fontWeight: "bold", fontSize: 18  }}>
-            Delete All
-           </Button>
-           <Button variant="contained" sx={{ pl: 4, pr: 4, mt: 3, mb: 2, mr: 2, background: "#62b1ea", "&:hover": { background: "#62bcca", opacity: [0.9, 0.8, 0.7] }, borderRadius: 25, borderColor: 'text.primary', fontWeight: "bold", fontSize: 18  }}>
-            Restore ALL
-          </Button>
-          <Button variant="contained" sx={{ pl: 4, pr: 4, mt: 3, mb: 2, mr: 5, background: "#68D391", "&:hover": { background: "#68D391", opacity: [0.9, 0.8, 0.7] }, borderRadius: 25, border: 1, borderColor: 'text.primary', color: "black", fontWeight: "bold", fontSize: 18 }}>
-            Back
-          </Button>
-        </Box>
-        <Box sx={{ display: "flex" }}>
+      <Box sx={{ display: "flex" }}>
           <Typography
             component="h1"
             variant="h4"
+            mt={3}
             mb={2}
             sx={{ fontWeight: "bold" }}
           >
             TRASH
           </Typography>
+          <Box sx={{ textAlign: "right", marginLeft: "auto" }} >
+            <Button variant="contained" sx={{ pl: 4, pr: 4, mt: 3, mb: 2, mr: 2, background: "#ec7272", "&:hover": { background: "#ec7171", opacity: [0.9, 0.8, 0.7] }, borderRadius: 25, borderColor: 'text.primary', fontWeight: "bold", fontSize: 18  }}>
+              Delete All
+            </Button>
+            <Button variant="contained" sx={{ pl: 4, pr: 4, mt: 3, mb: 2, mr: 2, background: "#62b1ea", "&:hover": { background: "#62bcca", opacity: [0.9, 0.8, 0.7] }, borderRadius: 25, borderColor: 'text.primary', fontWeight: "bold", fontSize: 18  }}>
+              Restore ALL
+            </Button>
+            <Button variant="contained" sx={{ pl: 4, pr: 4, mt: 3, mb: 2, mr: 5, background: "#68D391", "&:hover": { background: "#68D391", opacity: [0.9, 0.8, 0.7] }, borderRadius: 25, border: 1, borderColor: 'text.primary', color: "black", fontWeight: "bold", fontSize: 18 }}>
+              Back
+            </Button>
+          </Box>
         </Box>
         <Box>
-          <Box sx={{ display: 'flex', overflowX: "auto" }}>
+          <Box sx={{ display: "flex", flexFlow: "column", overflowX: "auto" }}>
             <Box sx={{  p: 2 }}>
               <TableContainer >
                 <Table sx={{ minWidth: 850 }} aria-label="simple table">
@@ -417,124 +418,15 @@ const detail: NextPage = () => {
                   </TableBody>
                 </Table>
               </TableContainer>
-            <Box component="span" 
-            sx={{ 
-              display: "flex",
-              mt: 5, 
-               }} 
-            />
-              <Button
-                size="small"
-                sx={{
-                  ml: 30,
-                  padding: 0,
-                  backgroundColor: "#b5b5b5",
-                  color: "white",
-                  "&:hover": {
-                    color: "#b5b5b5"
-                  }
-                }}
-              >
-                ＜
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  ml: 1,
-                  padding: 0,
-                  backgroundColor: "#fff",
-                  color: "#4c4c4c",
-                  border: 1,
-                  "&:hover": {
-                    backgroundColor: "#b5b5b5",
-                    color: "#fff"
-                  }
-                }}
-              >
-                1
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  ml: 1,
-                  padding: 0,
-                  backgroundColor: "#fff",
-                  color: "#4c4c4c",
-                  border: 1,
-                  "&:hover": {
-                    backgroundColor: "#b5b5b5",
-                    color: "#fff"
-                  }
-                }}
-              >
-                2
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  ml: 1,
-                  padding: 0,
-                  backgroundColor: "#fff",
-                  color: "#4c4c4c",
-                  border: 1,
-                  "&:hover": {
-                    backgroundColor: "#b5b5b5",
-                    color: "#fff"
-                  }
-                }}
-              >
-                …
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  ml: 1,
-                  padding: 0,
-                  backgroundColor: "#fff",
-                  color: "#4c4c4c",
-                  border: 1,
-                  "&:hover": {
-                    backgroundColor: "#b5b5b5",
-                    color: "#fff"
-                  }
-                }}
-              >
-                5
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  ml: 1,
-                  padding: 0,
-                  backgroundColor: "#fff",
-                  color: "#4c4c4c",
-                  border: 1,
-                  "&:hover": {
-                    backgroundColor: "#b5b5b5",
-                    color: "#fff"
-                  }
-                }}
-              >
-                6
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  ml: 1,
-                  padding: 0,
-                  backgroundColor: "#fff",
-                  color: "#4c4c4c",
-                  border: 1,
-                  "&:hover": {
-                    backgroundColor: "#b5b5b5",
-                    color: "#fff"
-                  }
-                }}
-              >
-                ＞
-              </Button>
-            <Box>
-            </Box>
+              <Box mt={6} mb={3} sx={{ textAlign: "center" }}>
+                <Pagination
+                  count={10}
+                  size="large"
+                  variant="outlined"
+                  shape="rounded"
+                  sx={{ display: "inline-block" }}
+                />
+              </Box>
           </Box>
         </Box>
       </Box>
