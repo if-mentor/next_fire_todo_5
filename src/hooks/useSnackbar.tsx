5623e9dc5ecab0d1e6b7f7bc8b387c3abedc26e4
tetsu633 <tetsunishimura633@gmail.com>
2022-06-08 23:36:06 +0900

    #37 Todoの編集機能 (#97)

---
+++ src/hooks/useSnackbar.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+import { Alert, Snackbar } from '@mui/material'
+
+const useSnackbar = () => {
+  const [message, setMessage] = useState<string | null>(null)
+
+  const closeSnackbar = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setMessage(null)
+  }
+
+  const AlertSnackbar: React.FC = ({ children, ...props }) => {
+    return (
+      <Snackbar
+        open={message !== null}
+        autoHideDuration={5000}
+        onClose={() => setMessage(null)}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        {...props}
+      >
+        <Alert onClose={closeSnackbar} severity="error">
+          { message }
+        </Alert>
+      </Snackbar>
+    )
+  }
+
+  return { setMessage, AlertSnackbar }
+}
+
+export default useSnackbar
--- src/pages/editTodo.tsx
+++ src/pages/editTodo.tsx
@@ -1,19 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 import { Container, FormLabel, OutlinedInput, Typography, Box, Stack, styled } from '@mui/material'
+import { CircularProgress } from '@mui/material'
+import { doc, getDoc, Timestamp, updateDoc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebaseConfig'
+import { parseTimestampToDate } from '../utils/DataFormat'
+import useSnackbar from '../hooks/useSnackbar'
+
+type Todo = {
+  id: string
+  title: string
+  detail: string
+  status: 'NOT STARTED' | 'DOING' | 'DONE'
+  priority: 'Low' | 'Middle' | 'High'
+  create: Timestamp
+  update: Timestamp
+  isDraft: boolean
+  author: string
+  editor: string
+}
+
+// firestore root directory
+const rootDir = 'todos'
 
 const EditTodo = () => {
-  // フォーム送信
+  const [todo, setTodo] = useState<Todo | null>(null)
+  const { setMessage, AlertSnackbar } = useSnackbar()
+  const router = useRouter()
+  const { id } = router.query
+
+  useEffect(() => {
+    // データ取得
+    if (id !== undefined) {
+      (async () => {
+        const docSnap = await getDoc(doc(db, rootDir, id as string))
+        docSnap.exists() && setTodo({ ...(docSnap.data() as Todo) })
+      })()
+    }
+  }, [id])
+
+  // データ更新
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
-    const data = new FormData(event.currentTarget)
-    console.log('title: ', data.get('title'))
-    console.log('detail: ', data.get('detail'))
+    // TITLEが0文字の場合のエラー
+    if (todo!.title.length === 0) {
+      setMessage('TITLEが入力されていません。')
+      return
+    }
+
+    // DETAILが0文字の場合のエラー
+    if (todo!.detail.length === 0) {
+      setMessage('DETAILが入力されていません。')
+      return
+    }
+
+    // TITLEが37文字以上の場合にエラー表示
+    if (todo!.title.length >= 37) {
+      setMessage('TITLEは37文字以上入力できません')
+      return
+    }
+
+    // DETIALが128文字以上の場合にエラー表示
+    if (todo!.detail.length >= 129) {
+      setMessage('DETAILは129文字以上入力できません')
+      return
+    }
+
+    if (id !== undefined && todo != null) {
+      (async () => {
+        await updateDoc(doc(db, rootDir, id as string), {
+          title: todo.title,
+          detail: todo.detail,
+          update: serverTimestamp(),
+          editor: 'userUID'
+        })
+      })().then(() => {
+        // Todoリストへ戻る
+        router.push('/')
+      })
+    }
   }
 
-  return (
-    <>
+  if (todo != null) {
+    return (
       <Container component="div">
         <Stack spacing={2} my={3}>
           <Box component="div" sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -26,6 +97,7 @@ const EditTodo = () => {
                 color: '#000',
                 '&:hover': { backgroundColor: '#9ae6b4' }
               }}
+              onClick={() => router.back()}
             >
               Back
             </SButton>
@@ -34,7 +106,14 @@ const EditTodo = () => {
             <Stack spacing={3}>
               <FormLabel sx={{ fontWeight: 600, fontSize: '20px' }}>
                 <div>TITLE</div>
-                <OutlinedInput id="title" name="title" fullWidth sx={{ fontWeight: 600, borderRadius: '10px' }} />
+                <OutlinedInput
+                  id="title"
+                  name="title"
+                  fullWidth
+                  value={todo.title}
+                  onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+                  sx={{ fontWeight: 600, borderRadius: '10px' }}
+                />
               </FormLabel>
               <FormLabel sx={{ fontWeight: 600, fontSize: '20px' }}>
                 <div>DETAIL</div>
@@ -43,6 +122,8 @@ const EditTodo = () => {
                   name="detail"
                   fullWidth
                   multiline
+                  value={todo.detail}
+                  onChange={(e) => setTodo({ ...todo, detail: e.target.value })}
                   sx={{
                     minHeight: '280px',
                     alignItems: 'start',
@@ -55,11 +136,15 @@ const EditTodo = () => {
                 <Box display="flex">
                   <Box mr={2}>
                     <Typography>Create</Typography>
-                    <p style={{ margin: '0 0 0 10px', fontWeight: 600 }}>2022-5-8 18:55</p>
+                    <p style={{ margin: '0 0 0 10px', fontWeight: 600 }}>
+                      {parseTimestampToDate(todo.create, '-', true)}
+                    </p>
                   </Box>
                   <Box>
                     <Typography>Update</Typography>
-                    <p style={{ margin: '0 0 0 10px', fontWeight: 600 }}>2022-5-9 20:00</p>
+                    <p style={{ margin: '0 0 0 10px', fontWeight: 600 }}>
+                      {parseTimestampToDate(todo.update, '-', true)}
+                    </p>
                   </Box>
                 </Box>
                 <SButton
@@ -76,9 +161,12 @@ const EditTodo = () => {
             </Stack>
           </form>
         </Stack>
+        <AlertSnackbar />
       </Container>
-    </>
-  )
+    )
+  } else {
+    return <CircularProgress />
+  }
 }
 
 // Button 共通スタイル
