5f2ab38607f609967dd9f379e9dc7628020db423
mogy-h <rapter.mg@gmail.com>
2022-06-11 16:29:28 +0900

    import先変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
