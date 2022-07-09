5f36970810fdb26dc51320684079a534c0f8156e
nto300002 <75260705+nto300002@users.noreply.github.com>
2022-07-09 16:57:41 +0900

    #45 todo詳細ページでのtodoへのコメント機能 (#125)
    * 機能追加:ログイン
    
    * fix: lint:tsxによるリファクタリング
    
    * add:forgotPassword.tsx usePasswordReset.tsx update:login.tsx forgotPassword.tsxへのリンク
    
    * fix:Loginバリデーション修正
    
    * fix:Loginバリデーション修正
    
    * add:詳細ページのコメント機能{#45}
    
    * add:コメント機能{#45}
    
    * add:draft機能{#115}
    
    * Delete draft.tsx
    
    * fix:add gravater{#45}
    
    * fix:draft.tsx作成{#45}
    
    Co-authored-by: YukiTakayama <yukitkym1011@gmail.com>

--- src/pages/draft.tsx
+++ src/pages/draft.tsx
@@ -34,11 +34,8 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import { db } from '../firebaseConfig'
 import { collection, doc, onSnapshot, orderBy, query, serverTimestamp, updateDoc, where } from 'firebase/firestore'
 import { parseTimestampToDate } from '../utils/DataFormat'
-import { useRecoilValue } from 'recoil'
-import { uidState } from '../atoms'
 
 const Draft: NextPage = () => {
-  const loginUid = useRecoilValue(uidState)
   const [todos, setTodos] = useState([
     {
       id: '',
@@ -47,7 +44,6 @@ const Draft: NextPage = () => {
       priority: '',
       create: '',
       update: '',
-      author: '',
       isDraft: true
     }
   ])
@@ -73,8 +69,7 @@ const Draft: NextPage = () => {
           create: parseTimestampToDate(todo.data().create, '-'),
           update: todo.data().update ? parseTimestampToDate(todo.data().update, '-') : '更新中',
           isDraft: todo.data().isDraft,
-          isTrash: todo.data().isTrash,
-          author: todo.data().author
+          isTrash: todo.data().isTrash
         }))
       )
     })
@@ -375,7 +370,6 @@ const Draft: NextPage = () => {
             <TableBody>
               {todos.map((todo: any) => {
                 if (
-                  todo.author === loginUid &&
                   todo.title.match(keyword) &&
                   (filteringStatus === todo.status || filteringStatus === 'NONE') &&
                   (filteringPriority === todo.priority || filteringPriority === 'None')
