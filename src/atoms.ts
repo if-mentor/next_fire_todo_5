6321fbff7d3f1a040d7f958416d828dd95992bcc
YukiTakayama <yukitkym1011@gmail.com>
2022-07-16 16:37:07 +0900

    yarn lint:tsx

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,19 +1,19 @@
-import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
 
 const { persistAtom } = recoilPersist({
-	key: "recoil-persist",
-	storage: typeof window === "undefined" ? undefined : sessionStorage
-});
+  key: 'recoil-persist',
+  storage: typeof window === 'undefined' ? undefined : sessionStorage
+})
 
 export const isLoginState = atom({
-	key: "isLoginState",
-	default: false,
+  key: 'isLoginState',
+  default: false,
   effects_UNSTABLE: [persistAtom]
-});
+})
 
 export const uidState = atom({
-	key: "uidState",
-	default: "",
+  key: 'uidState',
+  default: '',
   effects_UNSTABLE: [persistAtom]
-});
+})
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,15 +1,15 @@
-import { Timestamp } from "firebase/firestore"
+import { Timestamp } from 'firebase/firestore'
 
 export type Todo = {
-    id: string
-    title: string
-    detail: string
-    status: 'NOT STARTED' | 'DOING' | 'DONE'
-    priority: 'Low' | 'Middle' | 'High'
-    create: Timestamp
-    update: Timestamp
-    isDraft: boolean
-    isTrash: boolean
-    author: string
-    editor: string
+  id: string
+  title: string
+  detail: string
+  status: 'NOT STARTED' | 'DOING' | 'DONE'
+  priority: 'Low' | 'Middle' | 'High'
+  create: Timestamp
+  update: Timestamp
+  isDraft: boolean
+  isTrash: boolean
+  author: string
+  editor: string
 }
